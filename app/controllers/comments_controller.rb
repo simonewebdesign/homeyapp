c587f146e1a43ce7855a9bd8fa02026b422d19b2
Simone <hello@simonewebdesign.it>
2025-04-05 22:32:43 +0100

    Redirect to project after comment created

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,7 +26,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to [@project, @comment], notice: "Comment was successfully created." }
+        format.html { redirect_to @project, notice: "Comment was successfully created." }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
