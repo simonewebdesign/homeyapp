211d906d159dc82c6e51d196567f31be00c7e63d
Simone <hello@simonewebdesign.it>
2025-04-05 22:20:31 +0100

    Fix tests

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -2,47 +2,49 @@ require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    login_as users(:dave)
+    @project = projects(:one)
     @comment = comments(:one)
   end
 
   test "should get index" do
-    get comments_url
+    get project_comments_url(@project)
     assert_response :success
   end
 
   test "should get new" do
-    get new_comment_url
+    get new_project_comment_url(@project)
     assert_response :success
   end
 
   test "should create comment" do
     assert_difference("Comment.count") do
-      post comments_url, params: { comment: { content: @comment.content, project_id: @comment.project_id, user_id: @comment.user_id } }
+      post project_comments_url(@project), params: { comment: { content: @comment.content, project_id: @project.id, user_id: @comment.user_id } }
     end
 
-    assert_redirected_to comment_url(Comment.last)
+    assert_redirected_to project_comment_url(@project, Comment.last)
   end
 
   test "should show comment" do
-    get comment_url(@comment)
+    get project_comment_url(@project, @comment)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_comment_url(@comment)
+    get edit_project_comment_url(@project, @comment)
     assert_response :success
   end
 
   test "should update comment" do
-    patch comment_url(@comment), params: { comment: { content: @comment.content, project_id: @comment.project_id, user_id: @comment.user_id } }
-    assert_redirected_to comment_url(@comment)
+    patch project_comment_url(@project, @comment), params: { comment: { content: @comment.content, project_id: @project.id, user_id: @comment.user_id } }
+    assert_redirected_to project_comment_url(@project, @comment)
   end
 
   test "should destroy comment" do
     assert_difference("Comment.count", -1) do
-      delete comment_url(@comment)
+      delete project_comment_url(@project, @comment)
     end
 
-    assert_redirected_to comments_url
+    assert_redirected_to project_comments_url(@project)
   end
 end
