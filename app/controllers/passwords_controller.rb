949d19dc844677580ea7ea35094508ad913132c8
Simone <hello@simonewebdesign.it>
2025-04-06 21:14:08 +0100

    Disable mailer

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -7,7 +7,8 @@ class PasswordsController < ApplicationController
 
   def create
     if user = User.find_by(email_address: params[:email_address])
-      PasswordsMailer.reset(user).deliver_later
+      Rails.logger.info("User found: #{params[:email_address]}")
+      # PasswordsMailer.reset(user).deliver_later
     end
 
     redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
@@ -25,9 +26,10 @@ class PasswordsController < ApplicationController
   end
 
   private
-    def set_user_by_token
-      @user = User.find_by_password_reset_token!(params[:token])
-    rescue ActiveSupport::MessageVerifier::InvalidSignature
-      redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
-    end
+
+  def set_user_by_token
+    @user = User.find_by_password_reset_token!(params[:token])
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
+  end
 end
